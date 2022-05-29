d3fe6c061f2925a8626be1fb0fee4ea8543e7922
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-05-29 12:45:47 +0200

    Loading dotenv in config.py and removing key printing

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 import tweepy
 import logging
 import os
@@ -6,15 +7,11 @@ logger = logging.getLogger()
 
 
 def create_api():
+    load_dotenv()
     consumer_key = os.getenv("TWITTER_API_KEY")
     consumer_secret = os.getenv("TWITTER_API_KEY_SECRET")
     access_token = os.getenv("TWITTER_ACCESS_TOKEN")
     access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-
-    print(f"consumer key: {consumer_key}")
-    print(f"consumer secret: {consumer_secret}")
-    print(f"access token: {access_token}")
-    print(f"access token secret: {access_token_secret}")
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
